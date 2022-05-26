28697234934ceb2376e18d413eec1b69706e1656
Ken Robertson <kenluerobertson@hotmail.com>
2022-05-26 16:52:36 +0100

    elastic beanstalk config

--- main.tf
+++ main.tf
@@ -18,3 +18,26 @@ resource "aws_ecr_repository" "ken-repo" {
     scan_on_push = true
   }
 } 
+
+resource "aws_elastic_beanstalk_application" "ken_app" {
+  name        = "ken-task-listing-app"
+  description = "Task listing app"
+}
+
+resource "aws_elastic_beanstalk_environment" "ken_app_environment" {
+  name                = "ken-task-listing-app-environment"
+  application         = aws_elastic_beanstalk_application.ken_app.name
+  solution_stack_name = "64bit Amazon Linux 2 v3.4.5 running Docker"
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = aws_iam_instance_profile.ken_app_ec2_instance_profile.name
+  }
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name = "EC2KeyName"
+    value = "kenkeypair"
+  }
+}
