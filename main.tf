2a37a5237952431c51bb4f26eb276a2c17786f83
Ken Robertson <kenluerobertson@hotmail.com>
2022-05-26 14:07:31 +0100

    changes to main

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ terraform {
   }
 }
 
-resource "aws_ecr_repository" "kens-repo" {
-  name                 = "bar"
+resource "aws_ecr_repository" "ken-repo" {
+  name                 = "ken-terraform-repo"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
